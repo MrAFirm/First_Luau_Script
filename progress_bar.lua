83887df1db8c504eabcc78943fe73281a5734d14
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2025-05-24 19:20:07 +0800

    Update progress_bar.lua
    Managed to make the progress bar 10-100% and make the unicode code block increment as index increments. Made it print from 10-50%, then the percentages, followed by 60-100%, giving it a more clean look.

--- progress_bar.lua
+++ progress_bar.lua
@@ -1,10 +1,21 @@
 function incrementing()
-	local index = 1
+	local	index = 1
+	local	progress = 10
+	local	j = 6
+	local	block = "◼"
 	
-	while (index >= 1 and index <= 100) do
-		print("◼", index, "%")
-                wait(0.1)
+	while (index >= 1 and index <= 5) do
+		print(string.rep(block, index), progress, "%")
+		wait(0.1)
 		index += 1
+		progress += 10
+	end
+	index -= 1
+	while (j >= 6 and j <= 10) do
+		print(string.rep(block, index), progress, "%", string.rep(block, j - 5))
+		wait(0.1)
+		j += 1
+		progress += 10
 	end
 end
 
